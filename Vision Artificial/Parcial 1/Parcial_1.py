bdbc855d66fed2195f68edded0e5882a1a3c1a28
JulianNNN9 <111833091+JulianNNN9@users.noreply.github.com>
2025-03-06 21:14:19 -0500

    Arreglo en los imports

--- Vision Artificial/Parcial 1/Parcial_1.py
+++ Vision Artificial/Parcial 1/Parcial_1.py
@@ -11,42 +11,45 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.insert(0, parent_dir)
 
-from Funciones.Funciones import ChangeSize
-from Funciones.Funciones import InterpolationLinear
-from Funciones.Funciones import InterpolationNearest
-from Funciones.Funciones import And_Op
-from Funciones.Funciones import Or_Op
-from Funciones.Funciones import Not_Op
-from Funciones.Funciones import Sum_Op
-from Funciones.Funciones import Diff_Op
-from Funciones.Funciones import Mult_Op
-from Funciones.Funciones import Div_Op
-from Funciones.Funciones import ToBinary
-from Funciones.Funciones import ToGrayScale
-from Funciones.Funciones import RotateImage
+#Filtros (FALTA CANNY)
 from Funciones.Funciones import SharpenImage
 from Funciones.Funciones import RelieveImage
 from Funciones.Funciones import BlurFilter
 from Funciones.Funciones import CannyEdgeDetection
 from Funciones.Funciones import GaussianBlur
-from Funciones.Funciones import umbralizar_imagen
+
+#Filtros morfologicos
 from Funciones.Funciones import aplicar_apertura
 from Funciones.Funciones import aplicar_cierre
-from Funciones.Funciones import detectar_contornos
-from Funciones.Funciones import dibujar_contornos
-from Funciones.Funciones import calcular_caracteristicas
 from Funciones.Funciones import aplicar_gradiente
-from Funciones.Funciones import detectar_bordes_Canny
+
+#Cambios de Tonalidad (FALTAN ULTIMAS DOS)
+from Funciones.Funciones import ToBinary
+from Funciones.Funciones import ToGrayScale
+from Funciones.Funciones import umbralizar_imagen
+from Funciones.Funciones import umbralizacion_adaptativa
+
+#Operaciones Lógicas (FALTAN TODAS)
+from Funciones.Funciones import And_Op
+from Funciones.Funciones import Or_Op
+from Funciones.Funciones import Not_Op
+
+#Operaciones Matemáticas
+from Funciones.Funciones import Sum_Op
+from Funciones.Funciones import Diff_Op
+from Funciones.Funciones import Mult_Op
+from Funciones.Funciones import Div_Op
+
+#Ampliación y Reducción
+from Funciones.Funciones import ChangeSize
+from Funciones.Funciones import InterpolationLinear
+from Funciones.Funciones import InterpolationNearest
+
+#Segmentación (FALTA CANNY)
 from Funciones.Funciones import segmentacion_kmeans
 from Funciones.Funciones import segmentacion_watershed
-from Funciones.Funciones import umbralizacion_adaptativa
-import cv2
-import numpy as np
-import os
-import sys
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Button
-from matplotlib import get_backend
+from Funciones.Funciones import detectar_bordes_Canny
+
 
 # Ajustar rutas para importar funciones personalizadas
 current_dir = os.path.dirname(os.path.abspath(__file__))
