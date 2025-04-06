f21a68eaf971f311e35926be772af31d5a98f7e9
JulianNNN9 <111833091+JulianNNN9@users.noreply.github.com>
2025-04-06 17:29:02 -0500

    Cambio

---
+++ Vision Artificial/Parcial 2/funciones/funciones.py
@@ -0,0 +1,33 @@
+'''
+METODOS
+
+- Algoritmo AKAZE
+- Algoritmo SIFT
+- Algoritmo SURF
+- Algoritmo ORB
+- Algoritmo HOG (Histogram of Oriented Gradients)
+- Algoritmo KAZE
+- Transformada de HOUG
+    - Detección de Rectas
+    - Detección de Circunferencias
+- Métodos Estadísticos
+    - Estadísticos de Primer Orden
+        - Media
+        - Varianza
+        - Desviación
+        - Entropía
+    - Estadísticos de Segundo Orden
+        - Homogeneidad
+        - Contraste
+        - Disimilaridad
+        - Media
+        - Desviación Estándar
+        - Entropía
+        - Energía
+- Métodos Geométricos (No hay informacion en diapositivas)
+- Métodos basados en modelos (No hay informacion en diapositivas)
+- Métodos basados en tratamiento de señal (No hay informacion en diapositivas)
+
+'''
+
+print("a")
